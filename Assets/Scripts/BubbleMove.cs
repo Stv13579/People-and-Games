cde88f7ba90947be35041f51a89184710372542d
Seb <sebastian@thevalentines.net>
2021-05-31 13:50:55 +1200

    First Push
    Basic thought bubble inplementation

---
+++ Assets/Scripts/BubbleMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleMove : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.position = new Vector3(5 * Mathf.Cos(Time.time), this.transform.position.y + speed, this.transform.position.z); ;
+    }
+}
---
+++ Assets/Scripts/TextManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextManager : MonoBehaviour
+{
+    public int timer = 1000;
+    public GameObject bubble;
+    private GameObject currentBubble;
+    private GameObject currentBubbleCanvas;
+    private GameObject currentBubbleText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= 1;
+        if (timer <= 0)
+        {
+            timer = 1000;
+            currentBubble = Instantiate(bubble);
+            currentBubbleCanvas = currentBubble.transform.GetChild(0).gameObject;
+            currentBubbleText = currentBubbleCanvas.transform.GetChild(0).gameObject;
+            currentBubbleText.GetComponent<UnityEngine.UI.Text>().text = "test";
+        }
+    }
+}
