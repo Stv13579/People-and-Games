8ed05dfaafc205e72ceb442dfce0d8c3949e417e
Seb <sebastian@thevalentines.net>
2021-05-31 15:23:37 +1200

    General Imrpovements
    Tweaked movement, added a variable that increases when bubbles get too high, made bubbles disappear when they get too high, bubbles can now pull text from editable list, bubbles can now be dragged and dropped.

---
+++ Assets/Scripts/BubbleMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleMove : MonoBehaviour
+{
+    public float speed;
+    public GameObject textmanager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textmanager = FindObjectOfType<TextManager>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(this.transform.position.x + (0.01f * Mathf.Cos(Time.time)), this.transform.position.y + speed, this.transform.position.z);
+        if (this.transform.position.y > 5)
+        {
+            textmanager.GetComponent<TextManager>().score += 1;
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TextManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextManager : MonoBehaviour
+{
+    public int timer = 1000;
+    public string[] texts;
+
+    public GameObject bubble;
+    private GameObject currentBubble;
+    private GameObject currentBubbleCanvas;
+    private GameObject currentBubbleText;
+
+    public int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= 1;
+        if (timer <= 0)
+        {
+            timer = 1000;
+            currentBubble = Instantiate(bubble);
+            currentBubbleCanvas = currentBubble.transform.GetChild(0).gameObject;
+            currentBubbleText = currentBubbleCanvas.transform.GetChild(0).gameObject;
+            int index = Random.Range(0, texts.Length);
+            currentBubbleText.GetComponent<UnityEngine.UI.Text>().text = texts[index];
+        }
+    }
+}
